aeafd13eb3e4cea41aa665af760f6dd74080fd48
Ajith K.R <46483037+ajithkr12@users.noreply.github.com>
2022-06-25 11:21:49 +0530

    Update AchievementData.js

--- src/data/AchievementData.js
+++ src/data/AchievementData.js
@@ -5,7 +5,7 @@ export const ArcData =[
       "time": "2020",
       "dec1": "Selected in Top 15 Finalists of Reboot Kerala Hackathon.",
       "dec2": "Won 2nd prize in Regional Hackathon based on the theme LSGD.",
-      "dec3": "sana shaikh",
+      "dec3": "aaa",
     },
     {
         "heading": "Hack to the future 3",
@@ -13,7 +13,15 @@ export const ArcData =[
         "time": "2021",
         "dec1": "Won 1st Runner Up prize in Hack to the future 3.",
         "dec2": " Best student submission.",
-        "dec3": "sana shaikh",
+        "dec3": "aaa",
+    },
+        {
+        "heading": "Hack to the future 4",
+        "conducted": "Finastra",
+        "time": "2022",
+        "dec1": "Won 1st Runner Up prize in Hack to the future 3.",
+        "dec2": " Best student submission.",
+        "dec3": "aaa",
     },
 
 
