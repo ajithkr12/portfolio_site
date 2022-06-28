315efd90fcde68490354079de9ae8b8657cbd34e
Ajith K.R <46483037+ajithkr12@users.noreply.github.com>
2022-06-28 20:50:09 +0530

    Update ExperienceData.js

--- src/data/ExperienceData.js
+++ src/data/ExperienceData.js
@@ -3,11 +3,11 @@ export const ExpData =[
       "Time": "( SEP 2021 - Present )",
       "Company":"Tinqbit PVT LTD",
       "Position": "Reactjs developer",
-      "Details":"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat assumenda, eaque sequi repellat natus quia."
+      "Details":"Oversaw and assisted in the execution of front end works.Create reusable code for prospects.Translate wireframes and designs into good quality code."
     },
     {
       "Time": "( JULY 2015 - SEP 2017)",
-      "Company":"Foton Labs",
+      "Company":"Foton Labz",
       "Position": "Embedded system Designer",
       "Details":"Contributed to the development of a software test framework running on different µC in embedded target systems."
     },
