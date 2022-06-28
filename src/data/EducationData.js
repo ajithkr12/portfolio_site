cf4d45627d53e998e70a0b9e404c851c0e2d3541
Ajith K.R <46483037+ajithkr12@users.noreply.github.com>
2022-06-28 20:48:36 +0530

    Update EducationData.js

--- src/data/EducationData.js
+++ src/data/EducationData.js
@@ -4,28 +4,28 @@
       "Time": "( 2018 - 2021 )",
       "College": "College of Engineering, Kidangoor",
       "Course": "B-Tech-Electronics and communication",
-      "Mark":"CGPA : ",
+      "Mark":"CGPA : 7.0(still has 2 backlogs)",
       "Details":"Initiated & contributed to several experimental projects involving Embedded systemes, machine learning etc..... "
     },
     {
       "Time": "( 2012 - 2015 )",
       "College": "Govt. Polytechnic College Pala",
       "Course": "Diploma- Electronics Engineering",
-      "Mark":"CGPA : ",
+      "Mark":"CGPA : 8.1",
       "Details":"Initiated & contributed to several experimental projects involving IoT projects,etc.........."
     },
     {
       "Time": "( 2010 - 2012 )",
       "College": "NSS HSS Karukachal",
       "Course": "HSE - Computer Science",
-      "Mark":"CGPA : ",
+      "Mark":"Percentage : 73",
       "Details":"Amazing school experience at NSS HSS Karukachal."
     },
     {
       "Time": "( 2010 )",
       "College": "Dbhs Kangazha",
       "Course": "SSLC",
-      "Mark":"CGPA : ",
+      "Mark":"Percentage : 74",
       "Details":"Amazing school experience at DBHS Kangazha."
     },
 
